f9853df32bbc150b23ddddacebae200bb1fd5921
Juan miguel Bogado <bogadojuanmiguel@gmail.com>
2024-08-09 16:49:28 -0300

    [Feature] admin view added

--- controller/propiedadesControles.js
+++ controller/propiedadesControles.js
@@ -3,6 +3,7 @@ export const admin = (req, res ) => {
     res.render("propiedades/mis-propiedades",{
         titulo: "Mis Propiedades",
         pagina: "mis-popiedades",
+        barra: true
 
     })
 }
